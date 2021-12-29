62ea273f70c3465395c3994806712cdb67b501fd
Ganpat Parmar <84121179+ganpatparmar@users.noreply.github.com>
2021-12-29 18:56:32 +0530

    Update chaterbot.py

--- chaterbot.py
+++ chaterbot.py
@@ -1,62 +1,114 @@
-from chatterbot import ChatBot # importing chatterbot
-import logging
-from chatterbot.trainers import ChatterBotCorpusTrainer #impoting corpus trainer
-import speech_recognition as sr # for speech recognition 
-import pyttsx3
-import pywhatkit
-engine = pyttsx3.init()
+from chatterbot import ChatBot # Importing ChatBot Library
+from chatterbot.trainers import ChatterBotCorpusTrainer # Importing ChatBot Trainer Library to train our ChatBot
+import speech_recognition as sr # Importing speech_recongnition  Library for speech to text conversion
+import pyttsx3 # Importing pythtsx3  Library for conversation
+import pywhatkit # Importing pywhatkit Library for automatic web and Youtube task
+import os # # Importing OS Library for os related operations
+import subprocess
+
+''' creating an object for pyttsx3 and intialising it'''
+engine = pyttsx3.init() 
+''' creating an object for speech_recongnition and intialising it'''
 listener = sr.Recognizer()
 
 
-logging.basicConfig(level=logging.INFO)
-bot = ChatBot('walle', # creating instance by giving it a name
-    storage_adapters = 'chatterbot.storage.SQLStorageAdapter',#defining storage adapter
+''' creating an object for ChatBot and intialising it'''
+bot = ChatBot('ashu',
+    storage_adapters = 'chatterbot.storage.SQLStorageAdapter', # this adapter is SQL adapter for storing our conversations
     logic_adapters = [
-                      'chatterbot.logic.MathematicalEvaluation',# logic adapter for mathematical evaluation
-                        'chatterbot.logic.TimeLogicAdapter', # logic adapter for current time
-                        'chatterbot.logic.BestMatch', # logic adapter for selecting most appropriate response
-                       'chatterbot.logic.UnitConversion', # logic adapter for unit conversion
-                       #'chatterbot_weather.WeatherLogicAdapter'
-                        
-        
-    
-                        ],
-    preprocessors=[ 
-        'chatterbot.preprocessors.clean_whitespace'
-    ]
-            
-)
-trainer = ChatterBotCorpusTrainer(bot) #trainig the instance
+                      'chatterbot.logic.MathematicalEvaluation', # this adapter is Mathematical adapter for samll calculations
+                      'chatterbot.logic.BestMatch', # this adapter is for finding best match for our conversations
+                      'chatterbot.logic.UnitConversion'] # this adapeter is  for Unit conversations
+                      )
 
-trainer.train(
-    "chatterbot.corpus.english"
-)
+""" Training Our Bot  """
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train("chatterbot.corpus.english")
 
-print("Speak here something to begine.....")
-def speak(command): #method that will speak what input it recives
+""" Function that will speack any thing that you give as a command """
+def speak(command):
     engine = pyttsx3.init()
     engine.say(command) 
     engine.runAndWait()
 
-while True: # main code to generate responce by taking inputs. 
-    try:
-        with sr.Microphone() as source2:
-            print("You>> ")
-            listener.adjust_for_ambient_noise(source2, duration=0.2)
-            audio2 = listener.listen(source2)
-            user = listener.recognize_google(audio2)
-            user = user.lower()
+    
+""" Function for searching things for us """
+def search(command):
+    print('Bot>> Seacrhing....',command.join(command.split()[1:]))
+    speak('Searching ')
+    speak(command.split()[1:])
+    pywhatkit.search(command)
 
-            bot_responce = bot.get_response(user)
 
-            print('bot: ')
-            speak(bot_responce)
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
+""" Funtion for Playing things from YOUTUBE  """
+def play(command):
+    print('Bot>> Playing....',command.join(command.split()[1:]))
+    speak('Playing ')
+    speak(command.split()[1:])
+    pywhatkit.playonyt(command)
+
+
+""" Function for opening CAMERA MOUSE """
+def open(command):
+    path = "C:\Program Files (x86)\Camera Mouse\Camera Mouse 2018\Camera Mouse 2018.exe"
+    print('Bot>> opening....',command.join(command.split()[1:]))
+    speak('opening camera mouse')
+    os.startfile(path)
+    os.system(path)
+    subprocess.Popen([path])
+    subprocess.call(path)
+
+
+""" Fiunction for getting responce from bot """
+def bot_responce(command):
+    responce = bot.get_response(command)
+    print('Bot>> ',responce)
+    speak(responce)
+
+""" Function for listing stuff from user """
+def listen():
+    while True:
+        try:
+            with sr.Microphone() as source:
+               # print("Speack 'HEllO ASHU to start your' ")
+                #user = input("You>>")
+                listener.adjust_for_ambient_noise(source, duration=0.2)
+                audio = listener.listen(source)
+                user = listener.recognize_google(audio)
+                user = user.lower()
+                return user
+
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
           
-    except sr.UnknownValueError:
-        print("unknown error occured")
+        except sr.UnknownValueError:
+            print("unknown error occured")
 
-    except(SystemError,EOFError,KeyboardInterrupt):
-        break
+        except(SystemError,EOFError,KeyboardInterrupt):
+            break
 
+
+""" MAIN DRIVING CODE  """
+while True:
+    print("Say hellow ASHU to start your conversation :)")
+    user = listen()
+    if(user == 'hello ashu'):
+        speak('Hi buddy how can I help you')
+        while True:
+            print("speak:)")
+            user1 = listen()
+            print("You>> ",user1)
+            if(user1.split(' ')[0] == 'play'):
+                play(user1)
+            elif(user1.split(' ')[0] == 'search'):
+                search(user1)
+            elif(user1.split(' ')[0] == 'open'):
+                open(user1)                          
+            else:
+                bot_responce(user1)
+                
+    else:
+        continue
+            
+
+    
